95c1d8292300e6dadaa5cc0b1d9365f3fc570337
Roro-xd <rocioloportcuarto@gmail.com>
2025-03-25 14:34:50 +0100

    inicio raton y teclado

--- Assets/Scripts/Salto.cs
+++ Assets/Scripts/Salto.cs
@@ -11,14 +11,15 @@ public class Salto : MonoBehaviour
     public int numero2 = 80;
     public Vector3 posInicial = new Vector3(-1.75f, -0.5f, 0);
     public Color miColor; 
+    public float velocidad = 0.01f;
 
 
 
     void Start(){
 
-         this.GetComponent<Transform>().position = posInicial;
+        transform.position = posInicial;
 
-         this.GetComponent<SpriteRenderer>().flipX = true;
+        //this.GetComponent<SpriteRenderer>().flipX = true;
          
     }
 
@@ -35,22 +36,51 @@ public class Salto : MonoBehaviour
     // Update is called once per frame
     void Update(){
         
-        float posActual =  this.GetComponent<Transform>().position.x;
-        this.GetComponent<Transform>().position = new Vector3 (posActual+0.01f, -0.5f, 0);
+       //float posActual =  this.GetComponent<Transform>().position.x;
 
-        this.GetComponent<SpriteRenderer>().color = miColor;
+        //this.GetComponent<Transform>().position es = a transform.position
+        //transform.position = new Vector3 (posActual+0.01f, -0.5f, 0); 
+
+       //transform.Translate(velocidad, 0, 0);
+
+       //Debug.Log(transform.rotation);
+      //transform.Rotate(0, 0, velocidad);
+        
+        //this.GetComponent<SpriteRenderer>().color = miColor;
+
+        if(Input.GetKey(KeyCode.A)){ //Ir a la izquierda
+            transform.Translate(velocidad*-1, 0, 0);
+        }
+
+        if(Input.GetKey(KeyCode.D)){ //Ir a la derecha
+            transform.Translate(velocidad, 0, 0);
+        }
 
 
 
-
-        int suma = Sumar(numero1,numero2);
-        Debug.Log("La suma es: " + suma);
+        //int suma = Sumar(numero1,numero2);
+        //Debug.Log("La suma es: " + suma);
     }
 
-    int Sumar(int numero1, int numero2){
-        int resultado = numero1+numero2;
-        return resultado;
+
+
+
+    void OnGUI(){
+
+        if (Event.current.isMouse && Event.current.button == 0){
+            Debug.Log("awa");
+        }
+
     }
+
+
+
+
+
+    //int Sumar(int numero1, int numero2){
+        //int resultado = numero1+numero2;
+        //return resultado;
+    //}
 
 
 }
