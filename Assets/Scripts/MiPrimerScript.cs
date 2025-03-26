1c22c794349e5d270cf4ef0e26e2af37315c3337
Roro-xd <rocioloportcuarto@gmail.com>
2025-03-26 18:52:14 +0100

    adicion salto, flip, mejora mov

--- Assets/Scripts/MiPrimerScript.cs
+++ Assets/Scripts/MiPrimerScript.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+
+//ESTÁ DESACTIVADO EN EL PERSONAJE DE UNITY PARA QUE NO ME MOLESTE EL TEXTO MIENTRAS TRATO DE HACER LAS PRÁCTICAS
 public class MiPrimerScript : MonoBehaviour
 {
-    
-    public int miEdad = 21;
-    public float miAltura = 1.60f;
+    //Prueba de poner mi nombre (1: cración del string [la referencia])
     public string miNombre = "roro";
-    public bool llueve = false;
-    public GameObject miCubo;
 
-    //Prueba de comentario con líneas
+    //Prueba de hacer operaciones (1: creación de las variables)
+    //"Public" nos sirve para poder modificar los valores desde Unity durante el play
+    public int numero1 = 10;
+    public int numero2 = 80;
 
 
     void Start()
@@ -20,11 +21,26 @@ public class MiPrimerScript : MonoBehaviour
 
     }
 
-    // Update is called once per frame
+
+
     void Update()
     {
         
+        //Prueba de poner mi nombre (2: representación del string)
         Debug.Log("Hola, mi nombre es "+miNombre);
 
+        //Prueba de hacer operaciones (2: variable de la operación y representación)
+        int suma = Sumar(numero1,numero2);
+        Debug.Log("La suma es: " + suma);
+
     }
+
+
+
+    //Prueba de hacer operaciones (3: variable "Sumar" y establecimiento de resultado)
+    int Sumar(int numero1, int numero2){
+        int resultado = numero1+numero2;
+        return resultado;
+    }
+
 }
--- Assets/Scripts/Salto.cs
+++ Assets/Scripts/Salto.cs
@@ -4,83 +4,113 @@ using UnityEngine;
 
 public class Salto : MonoBehaviour
 {
-    // Start is called before the first frame update
-   
-   
-    public int numero1 = 10;
-    public int numero2 = 80;
+    //Movimiento (1: establezco la posición incial [sí o sí mediante un vector])
     public Vector3 posInicial = new Vector3(-1.75f, -0.5f, 0);
+
+    //Cambio de color (1: establezco la clase del color)
     public Color miColor; 
+
+    //Variación de velocidad (establezco la variable; es modificable durante el play)
     public float velocidad = 0.01f;
+
+    /*Cuerpo sólido (1: menciono la clase para rigidbody; creo un multiplicador para el salto;
+    var booleana para evitar saltos seguidos)*/
+    private Rigidbody2D rb;
+    public float multiplicadorSalto = 0.1f;
+    private bool puedoSaltar = true;
 
 
 
     void Start(){
 
+        //Movimiento (2: aclaro que la posición inicial se va a modificar)
         transform.position = posInicial;
 
-        //this.GetComponent<SpriteRenderer>().flipX = true;
+
+        /*en caso de querer que se gire, rote, etc. también se puede hacer así,
+        mediante "this.GetComponent<'VentanaDePropiedadesDeseada'>().'Propiedad' = 'ElValorDeseado'":
+            ---this.GetComponent<SpriteRenderer>().flipX = true;*/
          
+
+        //Cuerpo sólido (2: vincular la clase a la propiedad del personaje para que tenga acceso a ella)
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
 
 
+    //Otro posible método:
     void Awake(){
-
-
-
     }
 
 
 
-    // Update is called once per frame
     void Update(){
         
-       //float posActual =  this.GetComponent<Transform>().position.x;
+        /*Movimiento (3: FORMA MÁS EXTENSA de conseguir movimiento):
 
-        //this.GetComponent<Transform>().position es = a transform.position
-        //transform.position = new Vector3 (posActual+0.01f, -0.5f, 0); 
+            ·Establecimiento de variable de posición actual:
+                ---float posActual =  this.GetComponent<Transform>().position.x;
 
-       //transform.Translate(velocidad, 0, 0);
+            ·"this.GetComponent<Transform>().position" es igual que "transform.position"; FORMA MÁS SENCILLA:
+                ---transform.position = new Vector3 (posActual+0.01f, -0.5f, 0); 
 
-       //Debug.Log(transform.rotation);
-      //transform.Rotate(0, 0, velocidad);
-        
-        //this.GetComponent<SpriteRenderer>().color = miColor;
+            ·También puede rotar. "Debug.Log" es para ver el valor durante los cambios. "transform.Rotate('x','y','z')":
+                ---Debug.Log(transform.rotation);
+                ---transform.Rotate(0, 0, velocidad);*/
 
+
+
+        //Cambio de color (2: para poder modificar el color, accediendo a las propiedades)
+        this.GetComponent<SpriteRenderer>().color = miColor;
+
+
+        /*Movimiento (4: enlace del movimiento al input; "transform.position" y "transform.Translate"
+        son casi iguales; la última registra de por sí la posición actual=*/
         if(Input.GetKey(KeyCode.A)){ //Ir a la izquierda
             transform.Translate(velocidad*-1, 0, 0);
-        }
-
-        if(Input.GetKey(KeyCode.D)){ //Ir a la derecha
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        } else if(Input.GetKey(KeyCode.D)){ //Ir a la derecha
             transform.Translate(velocidad, 0, 0);
+            this.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        
+
+         /*Cuerpo sólido (3: coherencia de movimiento según el plano [terreno])
+            ---rb.velocity = new Vector2(multiplicadorSalto, rb.velocity.y);
+        en el vídeo usa movTeclas, nosotros no tenemos esa función*/
+
+        /*Cuerpo sólido (4: para que el personaje colisione con objetos + no pueda volver a saltar hasta que toque el SUELO;
+        "Raycast('origen', 'dirección', 'magnitud, longitud')")*/
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.5f);
+        Debug.DrawRay(transform.position, Vector2.down, Color.blue);
+
+        if (hit){
+            puedoSaltar = true;
+            Debug.Log(hit.collider.name);
+        }else { 
+            puedoSaltar = false;
         }
 
+        /*Cuerpo sólido (5: cuando pulsamos " ", le hace efecto una fuerza;
+        "AddForce('dirección' (vector de 2 valores solo), 'TipoDeFuerza' (impulso en nuestro caso))")*/
+        if(Input.GetKey(KeyCode.Space) && puedoSaltar){ //Saltar
 
+            rb.AddForce(new Vector2(0, multiplicadorSalto), ForceMode2D.Impulse);
+            puedoSaltar = false;
 
-        //int suma = Sumar(numero1,numero2);
-        //Debug.Log("La suma es: " + suma);
+        }
+        
     }
 
 
 
-
+    //GUI (para ver el texto en la consola; es solo para probar el ratón)
     void OnGUI(){
-
         if (Event.current.isMouse && Event.current.button == 0){
             Debug.Log("awa");
         }
-
     }
-
-
-
-
-
-    //int Sumar(int numero1, int numero2){
-        //int resultado = numero1+numero2;
-        //return resultado;
-    //}
 
 
 }
